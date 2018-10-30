1f4cdeaa237b01dbcba5c9b4c96172304e98aab3
luketis <luk.ties@gmail.com>
2018-10-30 01:58:14 -0200

    implemented post response

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -37,15 +37,40 @@ def index():
     label = 'Cigarros'
     #TEMP VALUES
 
-    print("ola")
     if request.method == 'POST':
-        print("hey fat cats")
-        url = request.form['url']
-        keywords = request.form['keywords']
-        force_calc = request.form.get('forcecalc')
 
-        print(url, keywords, force_calc)
+        if request.is_json:
+            in_json = request.get_json(force=True)
+            sites = in_json['sites']
+            callback = in_json['callback']
+            #TODO: call distributor
+
+        else:
+
+            url = request.form['url']
+            keywords = request.form['keywords']
+            force_calc = request.form.get('forcecalc')
+
+            print(url, keywords, force_calc)
+
+    else:
+        key = request.args.get('key')
+        veredict = request.args.get('veredict')
+        expl_words = request.args.get('expl_words')
+        label = request.args.get('label')
+
+        status = status_dict[key]
 
     return render_template('index.html', status=status, key=key,
                            veredict=veredict, expl_words=expl_words, label=label)
 
+
+
+import requests
+
+def answer(callback, result):
+    if callback is not None:
+        requests.post(callback, json=result)
+    else:
+
+        return redirect(url_for("index"), **result)
